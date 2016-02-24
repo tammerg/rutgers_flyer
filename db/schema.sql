acf2d22b0203353e6b05a73c2dc9c1106ad0c908
steph-harris <steph.harris21@gmail.com>
2016-02-24 15:37:12 -0500

    added commented out template for schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,18 @@
+### Schema
+
+-- CREATE DATABASE ;
+-- USE ;
+
+-- CREATE TABLE
+-- (
+-- id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+-- col_name varchar(255) NOT NULL
+-- );
+
+-- DESCRIBE ;
+
+-- INSERT INTO  (col_name) VALUES (?) ;
+
+-- SELECT * FROM ;
+
+--DROP DATABASE ;
