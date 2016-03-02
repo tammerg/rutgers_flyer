3727c0c78509500d7103d8e025c2c49107b1ab5a
tammerg <tammergalal20@gmail.com>
2016-03-02 12:51:52 -0500

    updated map to center on new brunswick at creation and added a marker at NBs location

--- public/scripts/googleMaps.js
+++ public/scripts/googleMaps.js
@@ -1,4 +1,14 @@
-map = new google.maps.Map(document.getElementById('map'), {
-  center: {lat: 40.4867, lng: 74.4444},
-  zoom: 8
-});
+function initMap(){
+  var myLatLng = {lat: 40.4862157, lng: -74.4518188};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: myLatLng,
+    zoom: 10,
+  });
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title:"EY YO!"
+  });
+}
+initMap();
