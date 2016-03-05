33f3117d1e0f957e8ab7224a5515554f09f16d01
vicrtor villacis <victor.villacis@outlook.com>
2016-03-05 04:30:23 -0500

    added filepicker api

--- public/scripts/components.js
+++ public/scripts/components.js
@@ -4,3 +4,14 @@ $(document).ready(function(){
   $('.datepicker').pickadate();
   $('select').material_select();
 });
+
+var yourApiKey = 'AbwDZpo3gTOyH18Lb4FHFz'
+filepicker.setKey(yourApiKey);
+
+
+function createImage() {
+    var img = document.createElement("img");
+    img.src = event.fpfile.url;
+    var tag = document.getElementById("results");
+    tag.appendChild(img);
+}
