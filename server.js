84e0d02e2ad0aec469e8a712dadba51eb5be6223
steph-harris <steph.harris21@gmail.com>
2016-03-04 22:06:01 -0500

    added working review delete button

--- server.js
+++ server.js
@@ -257,7 +257,17 @@ app.post("/update", function(req, res){
 });
 
 app.post("/remove", function(req, res){
-  console.log("Delete this?");
+  debugger
+  Review.findOne({
+    where: {
+      id:req.body.review_id,
+    }
+  }).then(function(thisReview){
+    console.log(thisReview);
+    thisReview.destroy().then(function(){
+      res.redirect("/userRevs");
+    })
+  });
 });
 
 
@@ -318,11 +328,10 @@ app.get('/userRevs', function(req, res){
       model: Review
     }]
   }).then(function(results){
-    debugger;
-    console.log(results.dataValues);
-    console.log(results.dataValues.reviews);
-    console.log(results.dataValues.reviews[0].dataValues);
-    console.log(results.dataValues.reviews[0].dataValues.review);
+    // console.log(results.dataValues);
+    // console.log(results.dataValues.reviews);
+    // console.log(results.dataValues.reviews[0].dataValues);
+    // console.log(results.dataValues.reviews[0].dataValues.review);
     res.render("reviews", {
       user:req.user,
       isAuthenticated: req.isAuthenticated(),
