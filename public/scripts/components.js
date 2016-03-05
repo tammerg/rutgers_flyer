eecab2bf076a516107f18feaf5376b36fd8ffc44
vicrtor villacis <victor.villacis@outlook.com>
2016-03-05 04:56:44 -0500

    made nav bar mobile responsive

--- public/scripts/components.js
+++ public/scripts/components.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
   $('.parallax').parallax();
   $('.modal-trigger').leanModal();
+  $('.button-collapse').sideNav();
   $('.datepicker').pickadate();
   $('select').material_select();
 });
